2a216434aef34983718dc0bdc885eab52cc3b4b7
John <johnnjau202@gmail.com>
2025-01-20 20:06:13 +0300

    methods POST

--- backend/main.py
+++ backend/main.py
@@ -10,9 +10,20 @@ def get_contacts():
     return jsonify({"contacts": json_contacts})
 
 
+@app.route("/create_contact", methods=["POST"]
+def create_contact():
+    first name = request.json.get("firstName")
+    last name = request.json.get("lastName")
+    email = request.json.get("email")
+
+    if not first_name or not last_name or not email:
+        return (
+            jsonify({"message": "You must include a first name, last name and email"}),
+            400,
+        )
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-
 
     app.run(debug=True)
