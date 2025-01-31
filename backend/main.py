6de14e7bc02ed48516cf6eb6b877494cf3a95230
John <johnnjau202@gmail.com>
2025-01-31 11:58:48 +0300

    debug_edits

--- backend/main.py
+++ backend/main.py
@@ -12,8 +12,8 @@ def get_contacts():
 
 @app.route("/create_contact", methods=["POST"])
 def create_contact():
-    first name = request.json.get("firstName")
-    last name = request.json.get("lastName")
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
     email = request.json.get("email")
 
     if not first_name or not last_name or not email:
